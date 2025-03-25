eb85a91f9abd575ead6a510603de0bc114b48ef3
sulthan <user@192.168.1.5>
2025-03-25 15:45:47 +0700

    built:feat/list-competition

--- src/page/competition/list-competition.jsx
+++ src/page/competition/list-competition.jsx
@@ -13,13 +13,11 @@ const ListCompetition = () => {
         <div className="flex flex-col items-center w-full h-full  absolute gap-2.5 z-10 pt-28">
           <img src={Piala} className="w-36 h-36" />
           <div className="flex gap-2.5">
-            <h1 className="font-bold font-normal text-5xl">CHALLANGE</h1>
-            <h2 className="font-bold font-normal text-5xl text-[#FDC833]">
-              YOUR SKILLS!
-            </h2>
+            <h1 className="font-bold text-5xl text-[#FFFAEB]">CHALLANGE</h1>
+            <h2 className="font-bold text-5xl text-[#FDC833]">YOUR SKILLS!</h2>
           </div>
           <div className="w-[620px] text-center">
-            <p className="font-normal font-medium text-2xl">
+            <p className="font-medium text-2xl">
               From Coding to Design, Join Various Exciting Competitions at
               Cakrawala Festival!
             </p>
