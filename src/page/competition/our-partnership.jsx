1450b8b8650d07780a7eef46a717138ea8653b5f
sulthan <user@192.168.1.5>
2025-03-26 13:27:40 +0700

    built:feat/prize-pool

--- src/page/competition/our-partnership.jsx
+++ src/page/competition/our-partnership.jsx
@@ -5,7 +5,7 @@ const Our_Partnership = () => {
     <div>
       {asset_partners.map((item, index) => {
         return (
-          <div key={index} className="col-md-4 ">
+          <div key={index} className="col-md-4">
             <img src={item.img_bg} className="w-full h-full" />
             <div className=" top-2 flex flex-col items-center ">
               <img src={item?.img_stars} className="" />
