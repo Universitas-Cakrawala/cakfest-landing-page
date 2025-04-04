68a47d92b99d489bc7b20e8cf68603f7b15d8b09
sulthan <user@192.168.1.9>
2025-04-04 19:07:12 +0700

    repair slick

--- src/page/competition/slick-slider/slick-list-competition.jsx
+++ src/page/competition/slick-slider/slick-list-competition.jsx
@@ -7,10 +7,10 @@ const Slick_List_Competition = () => {
     className: "center",
     centerMode: true,
     infinite: true,
-    centerPadding: "0px",
-    slidesToShow: 2,
+    centerPadding: "320px",
+    slidesToShow: 1,
     slideToScroll: 1,
-    autoplay: true,
+    autoplay: false,
     arrows: false,
     cssEase: "linear",
     autoplaySpeed: 3000,
@@ -27,14 +27,13 @@ const Slick_List_Competition = () => {
           const description = item?.description1;
           const img_bg_list = item?.img_bg_list;
           return (
-            <div key={index} className="flex justify-center">
+            <div key={index} className="relative h-[270px] w-[640px] px-5">
               <div className="relative w-full h-full">
                 <img
                   src={img_bg_list}
-                  className="mx-[345px] pl-[40px]"
-                  style={{ width: "100%", height: "100%" }}
+                  className="absolute inset-0 w-full h-full"
                 />
-                <div className="flex absolute top-8 left-[420px] gap-10 z-10">
+                <div className="flex gap-5 relative z-2 w-full h-full p-8">
                   <img src={img_logo_competition} />
                   <div className="flex flex-col w-full items-center gap-4">
                     <img
@@ -44,11 +43,34 @@ const Slick_List_Competition = () => {
                     <p className="font-normal text-xs text-center text-black w-[340px] h-[65px] overflow-hidden line-clamp-4">
                       {description}
                     </p>
-                    <Button_List_Competition item={item} />
+                    <Button_List_Competition />
                   </div>
                 </div>
               </div>
             </div>
+
+            // <div key={index} className="flex justify-center">
+            //   <div className="relative w-full h-full">
+            //     <img
+            //       src={img_bg_list}
+            //       className="mx-[345px] pl-[40px]"
+            //       style={{ width: "100%", height: "100%" }}
+            //     />
+            //     <div className="flex absolute top-8 left-[420px] gap-10 z-10">
+            //       <img src={img_logo_competition} />
+            //       <div className="flex flex-col w-full items-center gap-4">
+            //         <img
+            //           className="h-[70px] w-[190px]"
+            //           src={img_name_competition}
+            //         />
+            //         <p className="font-normal text-xs text-center text-black w-[340px] h-[65px] overflow-hidden line-clamp-4">
+            //           {description}
+            //         </p>
+            //         <Button_List_Competition item={item} />
+            //       </div>
+            //     </div>
+            //   </div>
+            // </div>
           );
         })}
       </Slider>
