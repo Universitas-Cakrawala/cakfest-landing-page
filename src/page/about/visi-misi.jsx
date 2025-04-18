6e89855d6589c654ccf45c4a2b6442cf0bc96e77
RugeFX <zackfxg@gmail.com>
2025-04-18 15:01:43 +0700

    fix(list-competition): fixed background sizing, use clipping mask instead of image asset

--- src/page/about/visi-misi.jsx
+++ src/page/about/visi-misi.jsx
@@ -14,18 +14,9 @@ const VisiMisi = () => {
       <Noted />
       <div className="relative w-full h-full">
         <picture>
-          <source
-            media="(max-width: 600px)"
-            srcSet={asset_about.img_bg_visi_misi_phone}
-          />
-          <source
-            media="(max-width: 1000px)"
-            srcSet={asset_about.img_bg_visi_misi_tablet}
-          />
-          <source
-            media="(min-width: 1001px)"
-            srcSet={asset_about.img_bg_visi_misi}
-          />
+          <source media="(max-width: 600px)" srcSet={asset_about.img_bg_visi_misi_phone} />
+          <source media="(max-width: 1000px)" srcSet={asset_about.img_bg_visi_misi_tablet} />
+          <source media="(min-width: 1001px)" srcSet={asset_about.img_bg_visi_misi} />
           <img
             src={asset_about.img_bg_visi_misi}
             className="absolute inset-0 w-full h-full object-cover object-bottom"
@@ -34,17 +25,14 @@ const VisiMisi = () => {
         </picture>
         <div className="flex flex-col items-center w-full h-full gap-8 relative z-10 py-20 px-4">
           <div className="flex flex-col items-center">
-            <img
-              src={asset_about.img_telescope}
-              className="size-[100px] sm:size-[120px]"
-            />
+            <img src={asset_about.img_telescope} className="size-[100px] sm:size-[120px]" />
             <h2 className="font-bold text-2xl sm:text-5xl text-center">
               <span className="text-[#FFFAEB]">OUR</span>{" "}
               <span className="text-[#FDC833]">VISION</span>
             </h2>
             <p className="w-full max-w-[620px] font-medium font-jakarta-sans text-base sm:text-2xl text-center pt-2.5">
-              Transforming Cakrawala University into a dynamic hub where
-              learning and play ignite innovation
+              Transforming Cakrawala University into a dynamic hub where learning and play ignite
+              innovation
             </p>
           </div>
           <div className="flex flex-col gap-2.5 justify-center">
@@ -53,7 +41,7 @@ const VisiMisi = () => {
               <span className="text-[#FDC833]">MISSION</span>
             </h2>
             <div className="relative w-full h-full pt-2.5 max-w-[1053px]">
-              <ul className="space-y-5 relative px-2 sm:px-12 py-10 bg-[#591266] pixel-corners">
+              <ul className="space-y-5 relative px-2 sm:px-12 py-10 bg-[#591266] pixel-corners-2">
                 <div className="w-full h-[15px] bg-[#9747A6] absolute top-0 left-0" />
                 <div className="w-8 h-[15px] bg-[#9747A6] absolute top-[15px] right-0" />
                 {MISSIONS.map((mission, index) => (
@@ -67,18 +55,10 @@ const VisiMisi = () => {
                       <rect width="40" height="40" fill="#D9D9D9" />
                       <rect x="30" width="10" height="20" fill="#C395CC" />
                       <rect y="20" width="10" height="20" fill="#C395CC" />
-                      <rect
-                        x="10"
-                        y="30"
-                        width="30"
-                        height="10"
-                        fill="#C395CC"
-                      />
+                      <rect x="10" y="30" width="30" height="10" fill="#C395CC" />
                       <rect width="30" height="10" fill="#C395CC" />
                     </svg>
-                    <p className="font-medium font-jakarta-sans text-base sm:text-2xl">
-                      {mission}
-                    </p>
+                    <p className="font-medium font-jakarta-sans text-base sm:text-2xl">{mission}</p>
                   </li>
                 ))}
               </ul>
--- src/page/competition/slick-slider/slick-list-competition.jsx
+++ src/page/competition/slick-slider/slick-list-competition.jsx
@@ -53,12 +53,7 @@ const Slick_List_Competition = () => {
     <div className="slider-container w-full overflow-hidden lg:px-32 py-10">
       <Slider {...settings} ref={sliderRef}>
         {asset_list_competition.map((item, index) => {
-          const {
-            img_name_competition,
-            img_logo_competition,
-            description1: description,
-            img_bg_list,
-          } = item;
+          const { img_name_competition, img_logo_competition, description1: description } = item;
 
           return (
             <div
@@ -67,21 +62,18 @@ const Slick_List_Competition = () => {
                 index === currentSlide ? "opacity-100" : "opacity-50"
               }`}
             >
-              <div className="relative w-full h-full">
-                <img
-                  src={img_bg_list}
-                  className="absolute inset-0 w-full h-full"
-                />
+              <div className="relative w-full h-full bg-[#F2E8F4] pixel-corners-3">
+                <div className="w-full h-[10px] bg-[#D7B8DD] absolute top-0 left-0" />
+                <div className="w-14 h-[20px] bg-[#D7B8DD] absolute top-[10px] right-0" />
+                <div className="w-full h-[10px] bg-[#C395CC] absolute bottom-0 left-0" />
+                <div className="w-14 h-[20px] bg-[#C395CC] absolute bottom-[10px] left-0" />
                 <div className="flex flex-col sm:flex-row justify-center items-center gap-2 lg:gap-5 relative z-2 w-full h-full p-8">
                   <img
                     src={img_logo_competition}
                     className="w-full h-fit aspect-square lg:max-w-[200px] max-w-[150px] shrink-0"
                   />
                   <div className="flex flex-col w-full h-full items-center gap-4">
-                    <img
-                      className="h-auto w-full max-w-[200px]"
-                      src={img_name_competition}
-                    />
+                    <img className="h-auto w-full max-w-[200px]" src={img_name_competition} />
                     <p className="font-normal font-jakarta-sans text-sm lg:text-base text-balance text-center text-black w-full h-full">
                       {description}
                     </p>
